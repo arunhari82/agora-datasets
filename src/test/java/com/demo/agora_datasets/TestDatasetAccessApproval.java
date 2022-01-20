7fa9e0e1830197e1ee2781d0a29f5429e2e0f22e
Diego Torres Fuerte <dtorresf@redhat.com>
2022-01-20 16:29:03 -0700

    preparing unit tests

--- src/test/java/com/demo/agora_datasets/TestDatasetAccessApproval.java
+++ src/test/java/com/demo/agora_datasets/TestDatasetAccessApproval.java
@@ -1,5 +1,25 @@
 package com.demo.agora_datasets;
 
-public class TestDatasetAccessApproval {
-  
+import org.jbpm.test.JbpmJUnitBaseTestCase;
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.process.ProcessInstance;
+
+public class TestDatasetAccessApproval extends JbpmJUnitBaseTestCase {
+  public TestDatasetAccessApproval() {
+    super(true, true);
+  }
+
+  @Test
+  public void testHelloWorld() {
+    createRuntimeManager("com/demo/agora_datasets/dataset_access_approval.bpmn");
+    RuntimeEngine runtimeEngine = getRuntimeEngine();
+    KieSession ksession = runtimeEngine.getKieSession();
+    ProcessInstance processInstance = ksession.startProcess("agora-datasets.dataset_access_approval");
+    assertProcessInstanceActive(processInstance.getId());
+    // assertProcessInstanceCompleted(processInstance.getId());
+    disposeRuntimeManager();
+  }
+
 }
