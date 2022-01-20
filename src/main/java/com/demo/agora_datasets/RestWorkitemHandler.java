8ee3820e75fc4d97bc3a76bc4fb2b1a809a5b457
Diego Torres Fuerte <dtorresf@redhat.com>
2022-01-20 13:51:06 -0700

    Empty work item handler definitions

---
+++ src/main/java/com/demo/agora_datasets/RestWorkitemHandler.java
@@ -0,0 +1,21 @@
+package com.demo.agora_datasets;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class RestWorkitemHandler implements WorkItemHandler {
+
+  @Override
+  public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
