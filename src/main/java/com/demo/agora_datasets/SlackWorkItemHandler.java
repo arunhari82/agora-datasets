3271bdb6188c7c80e6719ea2ee830f24fe9caee6
Arunkumar Nattamai Hariharan <anattama@redhat.com>
2022-01-25 13:26:06 -0500

    added Slack WIH without jSlack

--- src/main/java/com/demo/agora_datasets/SlackWorkItemHandler.java
+++ src/main/java/com/demo/agora_datasets/SlackWorkItemHandler.java
@@ -17,68 +17,48 @@ import org.jbpm.process.workitem.core.util.service.WidAuth;
 import org.jbpm.process.workitem.core.util.service.WidService;
 import org.jbpm.process.workitem.core.util.WidMavenDepends;
 
-import com.google.gson.JsonObject;
+import com.demo.agora_datasets.models.SlackMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-@Wid(widfile="SlackPostMessageDefinitions.wid", name="SlackPostMessage",
-        displayName="SlackPostMessage",
-        defaultHandler="mvel: com.demo.agora_datasets.SlackWorkItemHandler()",
-        documentation = "Slack/index.html",
-        category = "Slack",
-        icon = "SlackPostMessage.png",
-        parameters={
-                @WidParameter(name = "channelName", required = true),
-                @WidParameter(name = "message", required = true)
-        },
-        mavenDepends={
-        		@WidMavenDepends(group = "${groupId}", artifact = "${artifactId}", version = "${version}")
-        },
-        serviceInfo = @WidService(category = "${name}", description = "${description}",
-                keywords = "slack,message,send,channel",
-                action = @WidAction(title = "Send message to a slack channel"),
-                authinfo = @WidAuth(required = false, params = {"accessToken"},
-                        paramsdescription = {"Slack access token"},
-                        referencesite = "https://api.slack.com/tokens")
-        )
-)
-
+@Wid(widfile = "SlackPostMessageDefinitions.wid", name = "SlackPostMessage", displayName = "SlackPostMessage", defaultHandler = "mvel: com.demo.agora_datasets.SlackWorkItemHandler()", documentation = "Slack/index.html", category = "Slack", icon = "SlackPostMessage.png", parameters = {
+        @WidParameter(name = "channelName", required = true),
+        @WidParameter(name = "message", required = true)
+}, mavenDepends = {
+        @WidMavenDepends(group = "${groupId}", artifact = "${artifactId}", version = "${version}")
+}, serviceInfo = @WidService(category = "${name}", description = "${description}", keywords = "slack,message,send,channel", action = @WidAction(title = "Send message to a slack channel"), authinfo = @WidAuth(required = false, params = {
+        "accessToken" }, paramsdescription = { "Slack access token" }, referencesite = "https://api.slack.com/tokens")))
 
 public class SlackWorkItemHandler extends AbstractLogOrThrowWorkItemHandler {
-        private static final Logger logger = LoggerFactory.getLogger(SlackWorkItemHandler.class);
-        private String accessToken;
-        
-        public SlackWorkItemHandler()
-        {
-            this.accessToken = System.getProperty("slackAccessToken");
-            logger.info("*** using slack token **** " + this.accessToken);
-        }
+    private static final Logger logger = LoggerFactory.getLogger(SlackWorkItemHandler.class);
+    private String accessToken;
 
-       private void sendSlackMessage(String message) {
-           try 
-           {
-           String url = "https://hooks.slack.com/services/" + this.accessToken;
-           CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-           HttpPost request = new HttpPost(url);
-           request.addHeader("content-type","application/json");
-            JsonObject json = new JsonObject();
-            json.addProperty("text", message);
-            StringEntity entity = new StringEntity(json.toString());
+    public SlackWorkItemHandler() {
+        this.accessToken = System.getProperty("slackAccessToken");
+        logger.info("*** using slack token **** " + this.accessToken);
+    }
+
+    private void sendSlackMessage(String message) {
+        try {
+            String url = "https://hooks.slack.com/services/" + this.accessToken;
+            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost request = new HttpPost(url);
+            request.addHeader("content-type", "application/json");
+            SlackMessage slackMessage = new SlackMessage(message);
+            ObjectMapper mapper = new ObjectMapper();
+            String postBody = mapper.writeValueAsString(slackMessage);
+            StringEntity entity = new StringEntity(postBody);
             request.setEntity(entity);
             httpClient.execute(request);
-           }catch(Exception e)
-           {
-                   e.printStackTrace();;
-           }   
-       }
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+        }
+    }
 
-        
-
-
-
-        public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+    public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
         try {
             RequiredParameterValidator.validate(this.getClass(), workItem);
 
@@ -87,20 +67,18 @@ public class SlackWorkItemHandler extends AbstractLogOrThrowWorkItemHandler {
             String message = (String) workItem.getParameter("message");
 
             // complete workitem impl...
-                        
+
             // return results
             this.sendSlackMessage(message);
             manager.completeWorkItem(workItem.getId(), null);
-        } catch(Throwable cause) {
+        } catch (Throwable cause) {
             handleException(cause);
         }
     }
 
     @Override
     public void abortWorkItem(WorkItem workItem,
-                              WorkItemManager manager) {
+            WorkItemManager manager) {
         // stub
     }
 }
-
-
